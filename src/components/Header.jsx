944c2645be386e1561cbba1ce45cebb615b3b941
Andrea Marletta <andreamarletta7@gmail.com>
2025-05-27 15:41:20 +0200

    refactor: remove unnecessary <div> element and .div class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,7 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <header>
-      <div className={styles.div}>
-        <h1>TODO LIST</h1>
-      </div>
+      <h1 className={styles.title}>TODO LIST</h1>
       <form className={styles.form}>
         <p><label for="addTasks">Add a task</label></p>
         <p>

4dfefb050cc3e4b173dac0201c94a78db5f64f04
Andrea Marletta <andreamarletta7@gmail.com>
2025-05-28 18:00:27 +0200

    feat: implement toggleTodo functionality with onClick and completed style

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,9 @@
 import styles from './Todo.module.css';
 
-function Todo( { text, id, onDelete, completed } ) {
+function Todo( { text, id, onDelete, completed, onToggle } ) {
   return (
     <li className={completed ? styles.completed : styles.active}>
-      <input type="checkbox" id={id} />
+      <input type="checkbox" id={id} onClick={() => onToggle(id)} />
       {text}
       <button onClick={() => onDelete(id)}>Delete</button>
       <button>Edit</button>

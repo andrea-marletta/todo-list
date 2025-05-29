b258d4277ff6712c66d9261de969adc2eff965da
Andrea Marletta <andreamarletta7@gmail.com>
2025-05-29 16:39:01 +0200

    feat: return focus to edit button after editing in completed or canceled

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,32 @@
 import styles from './Todo.module.css';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function Todo( { text, id, onDelete, completed, onToggle, onSave } ) {
   const [isEditing, setIsEditing] = useState(false);
   const [editText, setEditText] = useState(text);
+  const editButtonRef = useRef(null);
+  const wasEditing = usePrevious(isEditing);
+
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    }, [value]);
+    return ref.current;
+  }
+
+  useEffect(() => {
+    if (wasEditing && !isEditing) {
+      editButtonRef.current?.focus();
+    }
+  }, [wasEditing, isEditing]);
 
   return (
     <li className={completed ? styles.completed : styles.active}>
       <input type="checkbox" id={id} onChange={() => onToggle(id)} />
       {text}
       <button onClick={() => onDelete(id)}>Delete</button>
-      <button onClick={() => setIsEditing(true)}>Edit</button>
+      <button onClick={() => setIsEditing(true)} ref={editButtonRef}>Edit</button>
       {isEditing && (
         <form
           onSubmit={(e) => {

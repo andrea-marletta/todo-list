fa7c0e4132c6ad57716128c784cb7e151dca8dcd
Andrea Marletta <andreamarletta7@gmail.com>
2025-05-29 15:55:51 +0200

    Refactor: use form onSubmit instead of button onClick to add tasks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,14 @@ function Header({ inputValue, setInputValue, onAddTodo }) {
   return (
     <header>
       <h1 className={styles.title}>TODO LIST</h1>
-      <form className={styles.form}>
+      <form 
+        className={styles.form} 
+        onSubmit={(e) => {
+          e.preventDefault();
+          onAddTodo();
+          }
+        }
+      >
         <p><label htmlFor="addTasks">Add a task</label></p>
         <p>
           <input 
@@ -14,7 +21,7 @@ function Header({ inputValue, setInputValue, onAddTodo }) {
             value={inputValue}
             className={styles.input} 
             onChange={(e) => setInputValue(e.target.value)} />
-          <button type="button" className={styles.button} onClick={onAddTodo}>
+          <button type="submit" className={styles.button}>
             + Add
           </button>
         </p>

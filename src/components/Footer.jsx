6bc059e6803ec57e254db03ebc262b7d72294992
Andrea Marletta <andreamarletta7@gmail.com>
2025-06-02 17:18:37 +0200

    docs: add MIT license link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,13 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <footer aria-label="Site Footer" className={styles.footer}>
-      <p>&copy; 2025 Andrea Marletta. Released under the MIT License.</p>
+      <p>&copy; 2025 Andrea Marletta. Released under the{ ' ' } 
+        <a 
+          href="https://github.com/andrea-marletta/todo-list/blob/main/LICENSE"   target="_blank" rel="noopener noreferrer">
+         MIT License
+        </a>
+      .
+      </p>
       <p>
         <a href="https://github.com/andrea-marletta/todo-list" target="_blank" rel="noopener noreferrer">
           View source on GitHub.
